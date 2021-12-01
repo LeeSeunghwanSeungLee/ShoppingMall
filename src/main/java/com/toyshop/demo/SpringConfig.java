268670dee02b1494748224f076053132bf045e10
lshn1007 <lshn1007@hanyang.ac.kr>
2021-12-02 00:54:16 +0900

    feat: 순수 JDBC 사용 Repository

--- src/main/java/com/toyshop/demo/SpringConfig.java
+++ src/main/java/com/toyshop/demo/SpringConfig.java
@@ -1,16 +1,30 @@
 package com.toyshop.demo;
 
+import com.toyshop.demo.repository.JdbcMemberRepository;
 import com.toyshop.demo.repository.MemberRepository;
 import com.toyshop.demo.repository.MemoryMemberRepository;
 import com.toyshop.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * description : 명시적으로 @Service || @Repository 로 등록하지 않고, 직접 등록해줄 수 있음
  */
 @Configuration
 public class SpringConfig {
+    /**
+     * JDBC를 이용해 데이터 베이스 연동을 위한 객체
+     */
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -18,6 +32,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
