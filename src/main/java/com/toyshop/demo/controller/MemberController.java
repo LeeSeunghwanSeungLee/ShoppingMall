bec0fa08d15350a9d2cd746fd671b1f4317869b0
lshn1007 <lshn1007@hanyang.ac.kr>
2021-12-02 00:33:49 +0900

    feat: MemberController 추가

---
+++ src/main/java/com/toyshop/demo/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.toyshop.demo.controller;
+
+import com.toyshop.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/com/toyshop/demo/service/MemberService.java
+++ src/main/java/com/toyshop/demo/service/MemberService.java
@@ -3,10 +3,12 @@ package com.toyshop.demo.service;
 import com.toyshop.demo.domain.Member;
 import com.toyshop.demo.repository.MemberRepository;
 import com.toyshop.demo.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
